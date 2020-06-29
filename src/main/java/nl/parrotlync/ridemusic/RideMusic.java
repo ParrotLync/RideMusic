92425ea7c75bb374f765dd33c920b4af75008bc3
ParrotLync <43622266+ParrotLync@users.noreply.github.com>
2020-06-29 22:59:28 +0200

    Add Metrics

--- src/main/java/nl/parrotlync/ridemusic/RideMusic.java
+++ src/main/java/nl/parrotlync/ridemusic/RideMusic.java
@@ -1,6 +1,7 @@
 package nl.parrotlync.ridemusic;
 
 import com.bergerkiller.bukkit.tc.signactions.SignAction;
+import nl.parrotlync.ridemusic.util.Metrics;
 import nl.parrotlync.ridemusic.util.UpdateChecker;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,6 +12,7 @@ public class RideMusic extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new RideListener(), this);
         this.getCommand("ridemusic").setExecutor(new RideCommandExecutor());
         SignAction.register(new RideSignAction());
+        Metrics metrics = new Metrics(this, 8038);
         getLogger().info("RideMusic is now enabled!");
         new UpdateChecker(this, 80710).getVersion(version -> {
             if (this.getDescription().getVersion().equalsIgnoreCase(version)) {
