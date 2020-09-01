3988153218def33ad34ecd59c3344dc2239f8a4d
ParrotLync <43622266+ParrotLync@users.noreply.github.com>
2020-09-01 16:04:24 +0200

    Bugfix in train detection

--- src/main/java/nl/parrotlync/ridemusic/RideSignAction.java
+++ src/main/java/nl/parrotlync/ridemusic/RideSignAction.java
@@ -3,11 +3,13 @@ package nl.parrotlync.ridemusic;
 import com.bergerkiller.bukkit.tc.events.SignActionEvent;
 import com.bergerkiller.bukkit.tc.events.SignChangeActionEvent;
 import com.bergerkiller.bukkit.tc.signactions.SignAction;
+import com.bergerkiller.bukkit.tc.signactions.SignActionType;
 import net.mcjukebox.plugin.bukkit.api.JukeboxAPI;
 import net.mcjukebox.plugin.bukkit.api.ResourceType;
 import net.mcjukebox.plugin.bukkit.api.models.Media;
 import nl.parrotlync.ridemusic.util.ChatUtil;
 import nl.parrotlync.ridemusic.util.DataUtil;
+import org.bukkit.Bukkit;
 
 import java.util.HashMap;
 
@@ -20,23 +22,24 @@ public class RideSignAction extends SignAction {
 
     @Override
     public void execute(SignActionEvent info) {
-        if (!info.isPowered()) { return; }
-        String name = info.getGroup().getProperties().getTrainName();
-        if (info.getLine(2).equalsIgnoreCase("music")) {
-            String url = String.valueOf(RideMusic.shortCodes.get(info.getLine(3)));
-            Media media = new Media(ResourceType.MUSIC, url);
-            media.setLooping(false);
-            JukeboxAPI.getShowManager().getShow(name).play(media);
-            return;
-        }
-        if (info.getLine(2).equalsIgnoreCase("sound")) {
-            String url = String.valueOf(RideMusic.shortCodes.get(info.getLine(3)));
-            Media media = new Media(ResourceType.SOUND_EFFECT, url);
-            JukeboxAPI.getShowManager().getShow(name).play(media);
-            return;
-        }
-        if (info.getLine(2).equalsIgnoreCase("stop")) {
-            JukeboxAPI.getShowManager().getShow(name).stopMusic();
+        if (info.isPowered() && info.isAction(SignActionType.GROUP_ENTER)) {
+            String name = info.getGroup().getProperties().getTrainName();
+            if (info.getLine(2).equalsIgnoreCase("music")) {
+                String url = String.valueOf(RideMusic.shortCodes.get(info.getLine(3)));
+                Media media = new Media(ResourceType.MUSIC, url);
+                media.setLooping(false);
+                JukeboxAPI.getShowManager().getShow(name).play(media);
+                return;
+            }
+            if (info.getLine(2).equalsIgnoreCase("sound")) {
+                String url = String.valueOf(RideMusic.shortCodes.get(info.getLine(3)));
+                Media media = new Media(ResourceType.SOUND_EFFECT, url);
+                JukeboxAPI.getShowManager().getShow(name).play(media);
+                return;
+            }
+            if (info.getLine(2).equalsIgnoreCase("stop")) {
+                JukeboxAPI.getShowManager().getShow(name).stopAll(2);
+            }
         }
     }
 
